9771f164194713408ed92ce37886db9124cec864
Louis Franceschi <louisbart3@gmail.com>
2024-02-04 22:10:33 +0100

    formatting

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+    std::cout << "********************\n";
     std::cout << "Select your option:\nSee your todos (a)\nAdd a new todo (b)\nDelete a todo (c)\n";
+    std::cout << "********************\n";
     return 0;
 }
